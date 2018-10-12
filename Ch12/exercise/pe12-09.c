c23c176c5111b86d1e8ec11753d696e245af246c
Donald Cheung <donald@localhost.localdomain>
2018-10-12 08:40:52 +0800

    Update

--- Ch12/exercise/pe12-09.c
+++ Ch12/exercise/pe12-09.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define SIZE 81
+
+int main(void)
+{
+    char *words;
+    char **ptd;
+    int n, i;
+
+    printf("How many words do you wish to enter? ");
+    scanf("%d", &n);
+    printf("Enter %d words now:\n", n);
+    words = (char *) malloc(SIZE * sizeof(char));
+    ptd = (char **) malloc(sizeof(char *) * n);
+
+    for (i = 0; i < n; i++)
+    {
+	scanf("%s", words);
+	int word_length= strlen(words);
+	ptd[i] = (char *)malloc(word_length + 1);
+	strcpy(ptd[i], words);
+    }
+
+    printf("Here are your words:\n");
+    for (i = 0; i < n; i++)
+    {
+	printf("%s\n", ptd[i]);
+    }
 
 
+    for (i = 0; i < n; i++)
+    {
+	free(ptd[i]);
+    }
+
+    return 0;
+}
