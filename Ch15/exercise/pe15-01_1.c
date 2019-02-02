df0913dd3d3d18b6086966284d17149bbe4b371e
matchaeggtart <matchaeggtart@hotmail.com>
2019-02-02 17:40:53 +0800

    Update

--- Ch15/exercise/pe15-01_1.c
+++ Ch15/exercise/pe15-01_1.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 #define SIZE 32
 
-char * s_gets(char * st, int n);
+char *s_gets(char *st, int n);
 bool check_bin(const char *st, int n);
-int btoi(const char * st);
+int btoi(const char *st);
 void cleanline(void);
 
 int main(void)
@@ -13,10 +13,10 @@ int main(void)
     char bin[SIZE];
     bool check;
     printf("Enter the binary up to 32 bits :\n");
-    
+
     while (s_gets(bin, SIZE) != NULL)
     {
-        
+
         while (!check_bin(bin, SIZE))
         {
             puts("Enter the binary up to 32 bits (0 and 1)");
@@ -24,41 +24,37 @@ int main(void)
         }
         if (bin[0] == '\0')
             break;
-        printf("%s = %d\n",bin, btoi(bin));
+        printf("%s = %d\n", bin, btoi(bin));
         printf("Enter the binary up to 32 bits :\n");
-        
     }
     puts("Bye!");
 
     return 0;
 }
 
-int btoi(const char * st)
+int btoi(const char *st)
 {
     int val = 0;
-    int count = 2;
-    int zero = 0;
+    int count, size;
+    ;
+    int dou = 2;
 
-for (count = 0; st[count] != '\0' && count < SIZE; count++)
-{
-    if (st[count] == '1')
+    for (size = 0; st[size + 1] != '\0'; size++)
+        ;
+    for (count = size - 1; count >= 0; count--)
     {
-        printf("true\n");
+        if (st[count] == '1')
+        {
+            val += dou;
+        }
+        dou *= 2;
     }
-    else
-    {
-        printf("wrong\n");
-    }
-    
-        val += count;
-    count *= 2;
-}
-
+    if (st[size] == '1')
+        val++;
     return val;
-    //return val;
 }
 
-bool check_bin(const char * st, int n)
+bool check_bin(const char *st, int n)
 {
     bool check = true;
     int count;
@@ -73,10 +69,10 @@ bool check_bin(const char * st, int n)
     return check;
 }
 
-char * s_gets(char * st, int n)
+char *s_gets(char *st, int n)
 {
-    char * find;
-    char * ret_val;
+    char *find;
+    char *ret_val;
 
     ret_val = fgets(st, n, stdin);
 
@@ -87,7 +83,7 @@ char * s_gets(char * st, int n)
             *find = '\0';
         else
             while (getchar() != '\n')
-                continue;        
+                continue;
     }
 
     return ret_val;
